ddbaa0f2ae5f888c6aa484456c3ddfc56276da55
duke <gj009351@163.com>
2020-06-18 00:01:53 +0800

    启动页动画

--- app/src/main/java/com/duke/yinyangli/activity/StartActivity.java
+++ app/src/main/java/com/duke/yinyangli/activity/StartActivity.java
@@ -1,11 +1,26 @@
 package com.duke.yinyangli.activity;
 
 import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.duke.yinyangli.R;
 import com.duke.yinyangli.base.BaseActivity;
 
+import butterknife.BindView;
+
 public class StartActivity extends BaseActivity {
+
+    private static final long DURATION_DELAY = 2000;
+    @BindView(R.id.logo)
+    ImageView logo;
+    @BindView(R.id.slogen)
+    TextView slogen;
 
     @Override
     public int getLayoutId() {
@@ -26,6 +41,13 @@ public class StartActivity extends BaseActivity {
                     }
                 });
             }
-        }, 0);
+        }, DURATION_DELAY);
+        Animation operatingAnim = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        LinearInterpolator lin = new LinearInterpolator();
+        operatingAnim.setInterpolator(lin);
+        logo.startAnimation(operatingAnim);
+//        YoYo.with(Techniques.RotateIn).duration(DURATION_DELAY / 2).pivotX(50).pivotY(50).repeatMode(1).playOn(logo);
+        YoYo.with(Techniques.Landing).duration(DURATION_DELAY / 2).repeatMode(-1).playOn(slogen);
     }
 }
---
+++ app/src/main/java/com/duke/yinyangli/view/GradientColorTextView.java
@@ -0,0 +1,37 @@
+package com.duke.yinyangli.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+public class GradientColorTextView extends AppCompatTextView {
+
+    private LinearGradient mLinearGradient;
+    private Paint mPaint;
+    private int mViewWidth = 0;
+    private Rect mTextBound = new Rect();
+
+    public GradientColorTextView (Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mViewWidth = getMeasuredWidth();
+        mPaint = getPaint();
+        String mTipText = getText().toString();
+        mPaint.getTextBounds(mTipText, 0, mTipText.length(), mTextBound);
+        mLinearGradient = new LinearGradient(0, 0, mViewWidth, 0,
+                new int[]{0xFF000000, 0xFFFFEABA, 0xFF000000},
+                null, Shader.TileMode.REPEAT);
+        mPaint.setShader(mLinearGradient);
+        canvas.drawText(mTipText, getMeasuredWidth() / 2 - mTextBound.width() / 2, getMeasuredHeight() / 2 + mTextBound.height() / 2, mPaint);
+    }
+}
