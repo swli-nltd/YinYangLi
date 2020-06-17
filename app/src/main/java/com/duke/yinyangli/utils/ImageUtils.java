8c6c616ad6cd91ee3557f7ad0b76796d7b602df8
duke <gj009351@163.com>
2020-06-18 00:36:16 +0800

    动画

---
+++ app/src/main/java/com/duke/yinyangli/utils/ImageUtils.java
@@ -0,0 +1,31 @@
+package com.duke.yinyangli.utils;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicBlur;
+import android.widget.ImageView;
+
+public class ImageUtils {
+
+    public static void setBlur(Context context, ImageView imageView, int resource) {
+        imageView.setImageBitmap(blur(context, BitmapFactory.decodeResource(context.getResources(), resource), 25));
+    }
+
+    public static Bitmap blur(Context context, Bitmap bitmap, float radius) {
+        Bitmap output = Bitmap.createBitmap(bitmap); // 创建输出图片
+        RenderScript rs = RenderScript.create(context); // 构建一个RenderScript对象
+        ScriptIntrinsicBlur gaussianBlue = ScriptIntrinsicBlur.create(rs, Element.U8_4(rs)); // 创建高斯模糊脚本
+        Allocation allIn = Allocation.createFromBitmap(rs, bitmap); // 创建用于输入的脚本类型
+        Allocation allOut = Allocation.createFromBitmap(rs, output); // 创建用于输出的脚本类型
+        gaussianBlue.setRadius(radius); // 设置模糊半径，范围0f<radius<=25f
+        gaussianBlue.setInput(allIn); // 设置输入脚本类型
+        gaussianBlue.forEach(allOut); // 执行高斯模糊算法，并将结果填入输出脚本类型中
+        allOut.copyTo(output); // 将输出内存编码为Bitmap，图片大小必须注意
+        rs.destroy(); // 关闭RenderScript对象，API>=23则使用rs.releaseAllContexts()
+        return output;
+    }
+}
